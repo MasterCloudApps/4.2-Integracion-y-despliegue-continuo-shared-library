5eb7fd43a1f34a86d97cb73da469ae1e1d0c3681
Patxi Gortázar <patxi.gortazar@gmail.com>
2023-06-09 18:10:27 +0200

    Fix hack

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,27 @@
-import es.urjc.code.mca.hello.Hello;
+// import es.urjc.code.mca.hello.Hello;
 
-def call(String name) {
-    echo new Hello().sayHello(name);
+// def call(String name) {
+//     echo new Hello().sayHello(name);
+// }
+def call() {
+
+    def mvnHome
+    
+    stage('Tooling') {
+        mvnHome = tool 'MAVEN3'
+    }
+
+    stage('Test') {
+        try {
+            sh "${mvnHome}/bin/mvn test"
+        }
+        finally {
+            junit 'target/surefire-reports/*.xml'
+        }
+    }
+
+    stage('Package') {
+        sh "${mvnHome}/bin/mvn -DskipTests clean package"
+        archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+    }
 }
