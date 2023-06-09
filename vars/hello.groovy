41f6ef2bfa48dd2c37f1347676680218a8951124
Patxi Gortázar <patxi.gortazar@gmail.com>
2023-06-09 18:14:36 +0200

    Revert fix hack

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,27 +1,5 @@
-// import es.urjc.code.mca.hello.Hello;
+import es.urjc.code.mca.hello.Hello;
 
-// def call(String name) {
-//     echo new Hello().sayHello(name);
-// }
-def call() {
-
-    def mvnHome
-    
-    stage('Tooling') {
-        mvnHome = tool 'MAVEN3'
-    }
-
-    stage('Test') {
-        try {
-            sh "${mvnHome}/bin/mvn test"
-        }
-        finally {
-            junit 'target/surefire-reports/*.xml'
-        }
-    }
-
-    stage('Package') {
-        sh "${mvnHome}/bin/mvn -DskipTests clean package"
-        archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
-    }
+def call(String name) {
+    echo new Hello().sayHello(name);
 }
