be32524de890618006eec9865ae6cb434cbb2bba
Patxi Gortázar <patxi.gortazar@gmail.com>
2023-06-09 18:04:53 +0200

    Fix mvn step, add mvn tool

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,7 +1,8 @@
 def call() {
     try {
-        sh "mvn test"
-        sh "mvn -DskipTests clean package"
+        def mvnHome = tool 'MAVEN3'
+        sh "${mvnHome}/bin/mvn test"
+        sh "${mvnHome}/bin/mvn -DskipTests clean package"
         archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
     }
     finally {
